57f6d2918e486d604810c0313a2491659964e0d7
MYRADhub <myradism@gmail.com>
2024-01-27 16:15:55 -0500

    landmark utils added

--- drawing_utils_custom.py
+++ drawing_utils_custom.py
@@ -116,6 +116,66 @@ def draw_detection(
                   bbox_drawing_spec.color, bbox_drawing_spec.thickness)
 
 
+def draw_landmark_from_array(
+        image: np.ndarray,
+        landmark_list: np.ndarray,
+        connections: Optional[List[Tuple[int, int]]] = None,
+        landmark_drawing_spec: Union[DrawingSpec,
+        Mapping[int, DrawingSpec]] = DrawingSpec(
+            color=RED_COLOR),
+        connection_drawing_spec: Union[DrawingSpec,
+        Mapping[Tuple[int, int],
+        DrawingSpec]] = DrawingSpec(),
+        is_drawing_landmarks: bool = True):
+    if not landmark_list:
+        return
+    if image.shape[2] != _BGR_CHANNELS:
+        raise ValueError('Input image must contain three channel bgr data.')
+    image_rows, image_cols, _ = image.shape
+    idx_to_coordinates = {}
+    delete_landmarks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 18, 19, 20, 21, 22]
+    for idx, landmark in enumerate(landmark_list.landmark):
+        if idx in delete_landmarks:
+            continue
+        if ((landmark.HasField('visibility') and
+             landmark.visibility < _VISIBILITY_THRESHOLD) or
+                (landmark.HasField('presence') and
+                 landmark.presence < _PRESENCE_THRESHOLD)):
+            continue
+        landmark_px = _normalized_to_pixel_coordinates(landmark.x, landmark.y,
+                                                       image_cols, image_rows)
+        if landmark_px:
+            idx_to_coordinates[idx] = landmark_px
+    if connections:
+        num_landmarks = len(landmark_list.landmark)
+        # Draws the connections if the start and end landmarks are both visible.
+        for connection in connections:
+            start_idx = connection[0]
+            end_idx = connection[1]
+            if not (0 <= start_idx < num_landmarks and 0 <= end_idx < num_landmarks):
+                raise ValueError(f'Landmark index is out of range. Invalid connection '
+                                 f'from landmark #{start_idx} to landmark #{end_idx}.')
+            if start_idx in idx_to_coordinates and end_idx in idx_to_coordinates:
+                drawing_spec = connection_drawing_spec[connection] if isinstance(
+                    connection_drawing_spec, Mapping) else connection_drawing_spec
+                cv2.line(image, idx_to_coordinates[start_idx],
+                         idx_to_coordinates[end_idx], drawing_spec.color,
+                         drawing_spec.thickness)
+    # Draws landmark points after finishing the connection lines, which is
+    # aesthetically better.
+    if is_drawing_landmarks and landmark_drawing_spec:
+        for idx, landmark_px in idx_to_coordinates.items():
+            drawing_spec = landmark_drawing_spec[idx] if isinstance(
+                landmark_drawing_spec, Mapping) else landmark_drawing_spec
+            # White circle border
+            circle_border_radius = max(drawing_spec.circle_radius + 1,
+                                       int(drawing_spec.circle_radius * 1.2))
+            cv2.circle(image, landmark_px, circle_border_radius, WHITE_COLOR,
+                       drawing_spec.thickness)
+            # Fill color into the circle
+            cv2.circle(image, landmark_px, drawing_spec.circle_radius,
+                       drawing_spec.color, drawing_spec.thickness)
+
 def draw_landmarks(
         image: np.ndarray,
         landmark_list: landmark_pb2.NormalizedLandmarkList,
@@ -157,7 +217,10 @@ def draw_landmarks(
         raise ValueError('Input image must contain three channel bgr data.')
     image_rows, image_cols, _ = image.shape
     idx_to_coordinates = {}
+    delete_landmarks = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 18, 19, 20, 21, 22]
     for idx, landmark in enumerate(landmark_list.landmark):
+        if idx in delete_landmarks:
+            continue
         if ((landmark.HasField('visibility') and
              landmark.visibility < _VISIBILITY_THRESHOLD) or
                 (landmark.HasField('presence') and
