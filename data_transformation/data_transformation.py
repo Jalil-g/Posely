64d8d7e3fc16396af5297dd104fa12e8bd0360b2
Jalil-g <jalil.jabbarli@mail.mcgill.ca>
2024-01-27 15:58:57 -0500

    Added PIL and CV2 support

--- data_transformation/data_transformation.py
+++ data_transformation/data_transformation.py
@@ -1,13 +1,24 @@
+from PIL import Image
 import cv2
+import numpy as np
 
-def resize_and_convert_to_bw_cv2(input_path):
-    # Read the image using OpenCV
-    original_image = cv2.imread(input_path)
+def resize_and_convert_to_bw_both(input_path):
+    # Open the image using Pillow
+    pil_image = Image.open(input_path)
 
-    # Resize the image to 480x480 pixels
-    resized_image = cv2.resize(original_image, (480, 480))
+    # Resize the image to 480x480 pixels using Pillow
+    resized_pil_image = pil_image.resize((480, 480))
 
-    # Convert the image to grayscale
-    bw_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+    # Convert the Pillow image to black and white
+    bw_pil_image = resized_pil_image.convert("L")
 
-    return bw_image
+    # Convert the Pillow image to a NumPy array for OpenCV
+    cv2_array = np.array(resized_pil_image)
+
+    # Resize the NumPy array to 480x480 pixels using OpenCV
+    resized_cv2_array = cv2.resize(cv2_array, (480, 480))
+
+    # Convert the OpenCV array to grayscale
+    bw_cv2_array = cv2.cvtColor(resized_cv2_array, cv2.COLOR_BGR2GRAY)
+
+    return bw_pil_image, bw_cv2_array
