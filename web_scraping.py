ff89324b03fe4a965103e75737e54efa8cc55724
Jalil10101 <jalil.jabbarli@mail.mcgill.ca>
2024-01-27 13:33:10 -0500

    scraper

---
+++ web_scraping.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+html_page = requests.get('http://books.toscrape.com/')
+soup = BeautifulSoup(html_page.content, 'html.parser')
+warning = soup.find('div', class_="alert alert-warning")
+book_container = warning.nextSibling.nextSibling
+
+images = book_container.findAll('img')
+example = images[0]
+example
