fc5d7ff7399db1fed487a549f977fb22830b02d7
abrarfuad27 <abrarfuad51@gmail.com>
2024-01-27 22:41:21 -0500

    add draw functionality

--- data_transformation/landmark_utils.py
+++ data_transformation/landmark_utils.py
@@ -4,8 +4,7 @@ import numpy as np
 
 mp_pose = mp.solutions.pose
 
-def generate_landmarks(image_path):
-    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+def generate_landmarks(image_path,land_arr):
         # open picture and represent it as an array
         image_frame = cv2.imread(image_path)
         image_frame = cv2.resize(image_frame, (480, 480))
@@ -13,25 +12,24 @@ def generate_landmarks(image_path):
         image_frame_rgb.flags.writeable = False
 
         # Make detection
-        processed = pose.process(image_frame_rgb)
 
         # Extract landmarks
-        try:
-            landmarks = processed.pose_landmarks.landmark
-        except:
-            print("---------------------LANDMARK NOT DETECTED ERROR----------------------")
-            return -1
+        # try:
+        #     landmarks = processed.pose_landmarks.landmark
+        # except:
+        #     print("---------------------LANDMARK NOT DETECTED ERROR----------------------")
+        #     return -1
 
-        landmark_list = []
-        delete_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 18, 19, 20, 21, 22, 29, 30, 31, 32]
-        for i in range(len(landmarks)):
-            if i not in delete_list:
-                if landmarks[i].visibility > 0.6:
-                    landmark_list.append([landmarks[i].x, landmarks[i].y, landmarks[i].z])
-                else:
-                    landmark_list.append([0, 0, 0])
+        # landmark_list = coordinates
+        # delete_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 17, 18, 19, 20, 21, 22, 29, 30, 31, 32]
+        # for i in range(len(landmarks)):
+        #     if i not in delete_list:
+        #         if landmarks[i].visibility > 0.6:
+        #             landmark_list.append([landmarks[i].x, landmarks[i].y, landmarks[i].z])
+        #         else:
+        #             landmark_list.append([0, 0, 0])
 
-        land_arr = np.array(landmark_list, dtype=np.float64) # Original picture landmarks
+        #land_arr = np.array(landmark_list, dtype=np.float64) # Original picture landmarks
 
         # Define connections between landmarks to represent a stickman
         connections = [
